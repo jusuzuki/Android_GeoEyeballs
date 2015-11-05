a77505a506f576c1151bc66730a600ffd3e61c9b
Perry Eising & Juliana Suzuki <student@epicodus.com>
2015-11-05 13:57:39 -0800

    upload images to Parse

--- app/src/main/java/com/example/guest/geoeyeballs/MainActivity.java
+++ app/src/main/java/com/example/guest/geoeyeballs/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.guest.geoeyeballs;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,8 +14,15 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     public static final int PICK_PHOTO_REQUEST = 2; //what does this mean?
+    public static final int MEDIA_TYPE_IMAGE = 4;
+
+    protected Uri mMediaUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,5 +85,61 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(choosePhotoIntent, PICK_PHOTO_REQUEST);
                 }
         });
+    }
+
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            if (requestCode == PICK_PHOTO_REQUEST){
+                if (data == null){
+                    Toast.makeText(getApplicationContext(),"PROBLEM DATA NULL",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mMediaUri = data.getData();
+                    try {
+                        Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mMediaUri);
+
+                        Log.i("AppInfo", "Image Received");
+
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                        bitmapImage.compress(Bitmap.CompressFormat.PNG, 10, stream);
+
+                        byte[] byteArray = stream.toByteArray();
+
+                        ParseFile file = new ParseFile("image.png", byteArray);
+
+                        ParseObject object = new ParseObject("Image");
+
+                        //object.put("username", ParseUser.getCurrentUser().getUsername());
+                        object.put("image", file);
+                        ParseACL parseACL = new ParseACL();
+                        parseACL.setPublicReadAccess(true);
+                        object.setACL(parseACL);
+
+                        object.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if( e == null) {
+                                    Toast.makeText(getApplication().getBaseContext(), "Your image has been posted", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(getApplication().getBaseContext(), "There was an error - please try again", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+                    } catch (IOException e) {
+                        Toast.makeText(getApplication().getBaseContext(), "There was an error - please try again", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }
+        else if (resultCode != RESULT_CANCELED){
+            Toast.makeText(getApplicationContext(),"PROBLEM",Toast.LENGTH_SHORT).show();
+        }
     }
 }
