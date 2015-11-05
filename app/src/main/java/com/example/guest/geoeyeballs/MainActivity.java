a757fa80f9d86d74fa2653bc016fc823ca4651b5
Perry Eising & Juliana Suzuki <student@epicodus.com>
2015-11-05 11:13:33 -0800

    Add logout button and show it only when user is logged in

--- app/src/main/java/com/example/guest/geoeyeballs/MainActivity.java
+++ app/src/main/java/com/example/guest/geoeyeballs/MainActivity.java
@@ -7,9 +7,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.signupActivityButton) Button signupActivityButton;
     @Bind(R.id.gotoLoginButton) Button gotoLoginButton;
+    @Bind(R.id.logoutButton) Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null){
+            logoutButton.setVisibility(View.VISIBLE);
+        }
 
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                Toast.makeText(getApplicationContext(),"You have been logged out",Toast.LENGTH_SHORT).show();
+                logoutButton.setVisibility(View.INVISIBLE);
+            }
+        });
 
         signupActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
